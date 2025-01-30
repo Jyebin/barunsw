2f7d91749aa51380c965018332ef99b5371ba18a
msyb0 <msyb0@192.168.0.3>
2025-01-30 20:05:06 +0900

    주소록 프로그램 구현 완료

--- OJT/src/main/java/day4/AddressBookInterface.java
+++ OJT/src/main/java/day4/AddressBookInterface.java
@@ -5,10 +5,11 @@ import java.util.List;
 public interface AddressBookInterface {
 	public List<AddressVO> selectAddressList(AddressVO addressVo) throws Exception;
 
-	public int insertAddress(AddressVO addressVo) throws Exception;
+	public void insertAddress(AddressVO addressVo) throws Exception;
 
-	public int updateAddress(AddressVO addressVo) throws Exception;
+	public void updateAddress(AddressVO addressVo) throws Exception;
 
-	public int deleteAddress(AddressVO addressVo) throws Exception;
+	public void deleteAddress(AddressVO addressVo) throws Exception;
 
 }
+ 
