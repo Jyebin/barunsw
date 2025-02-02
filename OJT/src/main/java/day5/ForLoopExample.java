b5ab5fd6d8bf1fc729a699e8a9a63ee09f037475
Jyebin <yebin0322@naver.com>
2025-02-02 22:51:42 +0900

    OJT

--- OJT/src/main/java/day5/ForLoopExample.java
+++ OJT/src/main/java/day5/ForLoopExample.java
@@ -1,10 +1,16 @@
 package day5;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class ForLoopExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		List<String> list = Arrays.asList("A", "B", "C", "D");
 
+		for (int i = 0; i < list.size(); i++) {
+			String element = list.get(i);
+			System.out.println(element);
+		}
 	}
-
 }
--- OJT/src/main/java/day5/IteratorExample.java
+++ OJT/src/main/java/day5/IteratorExample.java
@@ -1,16 +1,18 @@
-//package day5;
-//
-//import java.util.Arrays;
-//import java.util.Iterator;
-//import java.util.List;
-//
-//public class IteratorExample {
-//
-//	public static void main(String[] args) {
-//		List<String> list = Arrays.asList("A","B","C","D");
-//		Iterator<String> iterator = list.iterator();
-//		
-//		while()
-//	}
-//
-//}
+package day5;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+public class IteratorExample {
+
+	public static void main(String[] args) {
+		List<String> list = Arrays.asList("A", "B", "C", "D");
+		Iterator<String> iterator = list.iterator();
+
+		while (iterator.hasNext()) {
+			String element = iterator.next();
+			System.out.println(element);
+		}
+	}
+}
